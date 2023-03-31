d2b3a9c49f06100bebfd5458dfcf7d98b7133b3d
Wilson Syring <wilson.syring@gmail.com>
2023-03-31 13:07:00 -0500

    started working on the code but having issues woth the camera actually doing something

--- js/external.js
+++ js/external.js
@@ -1,5 +1,33 @@
+// html element query selectors
 const video = document.querySelector('.player');
 const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+//function for accessing the video camera and displaying it on the screen
+function getVideo(){
+    navigator.mediaDevices.getUserMedia( {video: true, audio: false})
+        .then(localMediaStream => {
+            console.log(localMediaStream);
+            video.src = window.URL.createObjectURL(localMediaStream);
+            video.play();
+        })
+        .catch(err => {
+            console.error(`Failed to load`, err);
+        });
+}
+
+//function for painting the video feed to the canvas
+function paintToCanvas(){
+    const width = video.videoWidth;
+    const height = video.videoHeight;
+    canvas.width = width;
+    canvas.height = height;
+
+    return setInterval(() => {
+        ctx.drawImage(video, 0,0, width, height);
+    }, 16);
+}
+
+getVideo();
